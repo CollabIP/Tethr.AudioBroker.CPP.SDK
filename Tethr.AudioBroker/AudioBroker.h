7f18f8ae543c63b841515631a8a3f1357cf4c93d
JTAtkins <jatkins@nerdplanet.com>
2017-05-08 15:35:32 -0500

    XML binaries
    Xml dependencies
    MTd build of open ssl doesnt link with vs2010 - Building OPENSSL
    manually

--- Tethr.AudioBroker/AudioBroker.h
+++ Tethr.AudioBroker/AudioBroker.h
@@ -6,14 +6,35 @@
 // that uses this DLL. This way any other project whose source files include this file see 
 // TETHRTEST_API functions as being imported from a DLL, whereas this DLL sees symbols
 // defined with this macro as being exported.
-#ifdef AUDIOBROKER_EXPORTS
-#define AUDIOBROKER_API __declspec(dllexport)
-#else
-#define AUDIOBROKER_API __declspec(dllimport)
+//
+// Ensure that AUDIOBROKER_DLL is default unless AUDIOBROKER_STATIC is defined
+//
+#if defined(_WIN32) && defined(_DLL)
+	#if !defined(AUDIOBROKER_DLL) && !defined(AUDIOBROKER_STATIC)
+		#define AUDIOBROKER_DLL
+	#endif
 #endif
 
-// This class is exported from the Tethr.Test.dll
-class AUDIOBROKER_API CAudioBroker {
+
+#if defined(_WIN32) && defined(AUDIOBROKER_DLL)
+	#if defined(AUDIOBROKER_EXPORTS)
+		#define AUDIOBROKER_API __declspec(dllexport)
+	#else
+		#define AUDIOBROKER_API __declspec(dllimport)
+	#endif
+#endif
+
+#if !defined(AUDIOBROKER_API)
+	#if !defined(AUDIOBROKER_NO_GCC_API_ATTRIBUTE) && defined (__GNUC__) && (__GNUC__ >= 4)
+		#define AUDIOBROKER_API __attribute__ ((visibility ("default")))
+	#else
+		#define AUDIOBROKER_API
+	#endif
+#endif
+
+
+// This class is exported from the Tethr.AudioBroker.dll
+/*class AUDIOBROKER_API CAudioBroker {
 public:
 	CAudioBroker(void);
 	// TODO: add your methods here.
@@ -21,7 +42,7 @@ public:
 
 inline CAudioBroker::CAudioBroker()
 {
-}
+}*/
 
 //Include Additional Headers
 #include <string>
